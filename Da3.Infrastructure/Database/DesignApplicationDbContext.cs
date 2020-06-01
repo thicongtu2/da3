904b66c1ef7d90bc1172b25dd52ad839991f342f
anh nguyen <anh.nguyen@east-gate.com>
2020-06-01 10:51:17 +0700

    remove bin

--- Da3.Infrastructure/Database/DesignApplicationDbContext.cs
+++ Da3.Infrastructure/Database/DesignApplicationDbContext.cs
@@ -8,7 +8,7 @@ namespace Da3.Infrastructure.Database
         public ApplicationDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            optionsBuilder.UseMySql("server=127.0.0.1;user id=root;password=password;port=13307;database=da3;");
+            optionsBuilder.UseMySql("server=127.0.0.1;user id=root;password=admin;port=3306;database=da3;");
             return new ApplicationDbContext(optionsBuilder.Options);
         }
     }
